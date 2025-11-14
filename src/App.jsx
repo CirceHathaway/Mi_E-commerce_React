194fa33459133a444fb9d5c007eb49ac2c1a660a
CirceHathaway <estebancarp72@gmail.com>
2025-11-14 03:08:33 -0300

    login admin + modificate json => api in products.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Footer } from "./components/Footer/Footer";
-import { Header } from "./components/Header/Header";
 import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { CartProvider } from "./context/CartContext/CartProvider";
 import { Cart } from "./components/Cart/Cart";
 import { ProductFormContainer } from "./components/adminComponents/ProductFormContainer/ProductFormContainer";
+import { MainLayout } from "./layouts/MainLayout";
+import { AdminLayout } from "./layouts/AdminLayout";
+import { RutaProtegida } from "./components/RutaProtegida/RutaProtegida";
+import { Login } from "./components/Login/Login";
 
 function App() {
   return (
@@ -14,20 +17,37 @@ function App() {
       <BrowserRouter>
         <CartProvider>
           <div>
-            <Header />
             {/* Dejamos fuera del Routes lo que queremos que no se vuelva a renderizar al navegar */}
             <Routes>
-              <Route
-                path="/"
-                element={<ItemListContainer titulo={"Bienvenidos"} />}
-              />
-              <Route
-                path="/category/:category"
-                element={<ItemListContainer titulo={"Bienvenidos"} />}
-              />
-              <Route path="/detail/:id" element={<ItemDetailContainer />} />
-              <Route path="/carrito" element={<Cart />}/>
-              <Route path="/admin" element={<ProductFormContainer />}/>
+
+              <Route element={<MainLayout />}>
+                <Route
+                  path="/"
+                  element={<ItemListContainer titulo={"Bienvenidos"} />}
+                />
+
+                <Route
+                  path="/category/:category"
+                  element={<ItemListContainer titulo={"Bienvenidos"} />}
+                />
+                <Route path="/detail/:id" element={<ItemDetailContainer />} />
+                <Route path="/carrito" element={<Cart />}/>
+              </Route>
+
+              <Route path="/admin" element={<AdminLayout />}>
+                <Route index element={<Login />} />
+
+                <Route 
+                  path="alta-productos" 
+                  element={
+                    <RutaProtegida>
+                      <ProductFormContainer />
+                    </RutaProtegida>
+                  }
+                />
+              </Route>
+              {/*<Route path="/admin" element={<ProductFormContainer />}/>*/}
+
             </Routes>
             {/* Dejamos fuera del Routes lo que queremos que no se vuelva a renderizar al navegar */}
             <Footer />
--- src/services/products.js
+++ src/services/products.js
@@ -14,3 +14,19 @@ export const createProduct = async (product) => {
   const result = await res.json();
   return result;
 };
+
+export const getProducts = async (category) => {
+  let url = BASE_URL;
+
+  if (category) {
+    url = `${BASE_URL}?category=${category}`;
+  }
+
+  const res = await fetch(url);
+  if (!res.ok){
+    throw new Error("Error al listar products");
+  }
+
+  const result = await res.json();
+  return result;
+}
